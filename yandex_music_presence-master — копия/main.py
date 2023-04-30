cb2de4768d6ab4580af4fd135e137391d7233a28
olegatronik12 <64388676+olegatronik12@users.noreply.github.com>
2023-04-30 16:02:19 +0900

    Add files via upload

---
+++ yandex_music_presence-master — копия/main.py
@@ -0,0 +1,55 @@
+import time
+
+from pypresence import Presence
+from yandex_music import Client
+
+from load_config import Y_MUSIC_KEY, CLIENT_ID
+
+
+def main():
+    client = Client(Y_MUSIC_KEY).init()
+
+    RPC = Presence(CLIENT_ID)
+    RPC.connect()
+
+    cur_track_id = 0
+    started = time.time()
+    artists = "None"
+    title = "None"
+    img_uri = "https://yastatic.net/s3/doc-binary/freeze/r-kLVqy2op9wYtE_g9RaJ8lzNtY.png"
+    buttons = None
+
+    while True:
+        try:
+            queues = client.queues_list()
+            last_queue = client.queue(queues[0].id)
+            last_track_id = last_queue.get_current_track()
+            if last_track_id != cur_track_id:
+                cur_track_id = last_track_id
+                last_track = last_track_id.fetch_track()
+                started = time.time()
+                artists = ', '.join(last_track.artists_name())
+                title = last_track.title
+                img_uri = f"https://{last_track.cover_uri[:-2]}400x400"
+                buttons = [{"label": "Слушать", "url": f"https://music.yandex.ru/track/{last_track.id}"}]
+                print(f"{artists} - {title}")
+            RPC.update(
+                state=f"{artists} - {title}",
+                large_image=img_uri,
+                start=int(started),
+                buttons=buttons
+            )
+            time.sleep(1)
+        except Exception as ex:
+            RPC.update(
+                state=f"Listening radio",
+                large_image=img_uri,
+                start=int(started),
+                buttons=buttons
+            )
+            excep = " current_index "
+            if ex == "TypeError":
+                print(ex)
+            else:
+                print("Ублюдок, у тебя радио играет")
+            time.sleep(1)
